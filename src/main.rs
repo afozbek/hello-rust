a960c0353a0c3632097be9d9018f09df1c9bf33d
Furkan Ozbek <abdullah.furkan.ozbek@gmail.com>
2022-09-13 12:57:08 +0300

    hello-world pushed

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    println!("Hello, world!");
-    // print_numbers_to(5)
-    let typed_numbers: [i32; 5] = [1, 2, 3, 4, 5];
-    for i in 0..typed_numbers.len() {
-        println!("{}", typed_numbers[i])
-    }
+    hello_world();
+}
+
+fn hello_world() {
+    println!("Hello World!");
 }
 
 fn print_numbers_to(number: u32) {
@@ -15,4 +14,22 @@ fn print_numbers_to(number: u32) {
 
 fn is_even(number: u32) -> bool {
     return number % 2 == 0;
+}
+fn listNumbers() {
+    let typed_numbers: [i32; 5] = [1, 2, 3, 4, 5];
+    for i in 0..typed_numbers.len() {
+        println!("{}", typed_numbers[i])
+    }
+}
+
+fn test_vectors() {
+    let mut v = vec![1, 2, 3, 4, 5];
+
+    v.push(1);
+    v.push(2);
+    v.push(3);
+
+    let first = &v[0];
+
+    println!("The first element is: {}", first);
 }
