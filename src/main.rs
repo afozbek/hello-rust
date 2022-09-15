6c677198ea775d8a21abd885461a0293524e5da2
Furkan Ozbek <abdullah.furkan.ozbek@gmail.com>
2022-09-15 11:15:07 +0300

    match operator test added

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,11 @@ fn main() {
 fn hello_world() {
     println!("Hello World!");
 }
-
 fn print_numbers_to(number: u32) {
     for n in 1..number {
         println!("{}", is_even(n));
     }
 }
-
 fn is_even(number: u32) -> bool {
     return number % 2 == 0;
 }
@@ -21,7 +19,6 @@ fn listNumbers() {
         println!("{}", typed_numbers[i])
     }
 }
-
 fn test_vectors() {
     let mut v = vec![1, 2, 3, 4, 5];
 
@@ -32,4 +29,11 @@ fn test_vectors() {
     let first = &v[0];
 
     println!("The first element is: {}", first);
+}
+fn test_match_operator(number: i32) {
+    match number {
+        1 => println!("Number is one"),
+        2 => println!("Number is two"),
+        _ => println!("I dont know!!"),
+    }
 }
